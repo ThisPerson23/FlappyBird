6ad8280d46989d292c1dad738c9dfc8e56eaa2ad
Justin Lange <j.lange@gmx.com>
2019-04-11 20:08:50 -0300

    Changed Splash Screen Background

--- FlappyBird/DEFINITIONS.h
+++ FlappyBird/DEFINITIONS.h
@@ -5,7 +5,7 @@
 
 #define SPLASH_STATE_SHOW_TIME 1.0
 
-#define SPLASH_SCENE_BACKGROUND_FILEPATH "Resources/res/Splash Background.png"
+#define SPLASH_SCENE_BACKGROUND_FILEPATH "Resources/res/sky.png"
 #define MAIN_MENU_BACKGROUND_FILEPATH "Resources/res/sky.png"
 #define GAME_BACKGROUND_FILEPATH "Resources/res/sky.png"
 #define GAME_OVER_BACKGROUND_FILEPATH "Resources/res/sky.png"
